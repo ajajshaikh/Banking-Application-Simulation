7576ef5bdd1e351558f63a4ab82220d7b48b46b1
ajajshaikh <45158660+ajajshaikh@users.noreply.github.com>
2018-12-03 12:26:32 +0530

    Update TransactionRepository.java

--- src/main/java/com/ajaj/repository/TransactionRepository.java
+++ src/main/java/com/ajaj/repository/TransactionRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ajaj.model.Transaction;;
-
+@Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
 
 	@Query("select f from Transaction f")
